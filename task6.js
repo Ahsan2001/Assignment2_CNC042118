4d9d2c936b500c53ed63b70af7aefbf8c0f6892a
Muhammad Ahsan <55804800+Ahsan2001@users.noreply.github.com>
2020-11-18 05:26:57 +0500

    Added Task Files

---
+++ task6.js
@@ -0,0 +1,11 @@
+// Write a program that checks whether the given input is an even
+// number or an odd number.
+
+var givenNumber = Number(prompt("Enter Number to Check Even or Odd"));
+
+if(givenNumber % 2 == 0) {
+    document.write("<h1>Even Number.......!!</h1>");
+ } 
+else {
+    document.write("<h1>Odd Number.......!!</h1>");
+ }

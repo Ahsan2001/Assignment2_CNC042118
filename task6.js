46744c70bd650c0b53645db0ef6f32c512cf7f58
Muhammad Ahsan <55804800+Ahsan2001@users.noreply.github.com>
2020-11-23 04:39:51 +0500

    Update task6.js

--- task6.js
+++ task6.js
@@ -9,3 +9,20 @@ if(givenNumber % 2 == 0) {
 else {
     document.write("<h1>Odd Number.......!!</h1>");
  }
+
+//Sir Zeeshan Code 
+//Prime Number Example
+
+var givenNumber = parseInt(prompt("Enter Number to check prime number"));
+var isPrime = true;
+for (var i=2 , i < givenNumber, i++){
+    var result = givenNumber % 2;
+    if (result == 0){
+        console.log("Number is not prime");
+        isPrime = false;
+        break;
+    }
+}
+if (isPrime){
+    console.log("Number is Prime");
+}
